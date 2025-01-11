5bb8ecd53f39fbc5986f699da190a9f4682868bf
Ismail <dewdigitalgr@gmail.com>
2025-01-12 00:33:19 +0400

    Initial commit

--- app/products/healthcare_accessibility/page.tsx
+++ app/products/healthcare_accessibility/page.tsx
@@ -33,21 +33,19 @@ const page = () => {
         
       </div>
 
-      <div className='flex lg:flex-row flex-col lg:gap-20 lg:m-20 items-center  justify-center'>
+      <div className='flex lg:flex-row flex-col lg:gap-20 lg:my-40 lg:mx-20 items-center  justify-center'>
           <div className='lg:w-1/2'>
           <div className='flex flex-col gap-8 p-12'>
-          <p className='font-normal text-sm text-accent-3/30 '>Products & Services</p>
-          <h2 className='font-bold text-3xl md:text-5xl  text-accent-3/100'>The <span className='text-Primary-1/70 underline'>Copper</span> Advantage</h2>
-          <p className='font-thin text-sm text-accent-3/30 '>Shaping a Sustainable Future with Copper: Powering Innovation in Energy and Electronics</p>
+          <p className='font-normal text-sm text-accent-3/30 '>Mission</p>
+          <h2 className='font-bold text-3xl md:text-5xl dark:text-white text-accent-3/100'>Empowering <span className='text-Primary-1/70 underline'>Healthcare</span> Systems</h2>
+          <p className='font-thin text-sm text-accent-3/30 '>Enhancing healthcare with innovative solutions for Africa and the Middle East.</p>
           </div>
           
-          <div className=' px-10 lg:px-12'> <p className='font-normal text-md text-accent-3/70 '>Copper is more than just a metal. Its unparalleled electrical and thermal conductivity, corrosion resistance, and malleability make it indispensable for power generation, electronics, and new energy technologies like electric vehicles (EVs). Our work ensures this critical resource powers a sustainable and efficient future.</p>
-          </div>
-
-          
+          <div className=' px-10 lg:px-12'> <p className='font-normal text-md text-accent-3/70 '>We aim to enhance healthcare delivery by supplying innovative and essential medical products that empower hospitals, clinics, and healthcare organizations. By addressing critical healthcare challenges, we strive to create healthier communities and support the development of robust healthcare systems across Africa and the Middle East.</p>
           </div>
 
           
+          </div>     
 
           
 
@@ -58,66 +56,38 @@ const page = () => {
                 height={600}
                 alt="copper image"
               />
-          </div>
-          
-
-          
+          </div>     
           
       </div>
 
 
-      <div className='md:m-10 m-8 flex flex-col sm:flex-row md:gap-4 bg-Primary-1/10 rounded-[20px]'>
-        <div className='md:p-20 p-4 m-2 gap-8 flex flex-col justify-center'>
-          <div className='flex gap-4 flex-col'>
-            <h2 className='text-5xl font-bold lg:text-left text-center'>Our Expertise</h2>
-            <h4 className='text-xl lg:text-left text-center'>From Mine to Market: Comprehensive Copper Solutions</h4>
-          </div>
-
-          <div className='flex lg:flex-row flex-col gap-8 '>
-            <div className='flex flex-col lg:w-1/2 bg-gray-100 p-8 rounded-2xl '>
-              <div className='lg:p-8'> 
-                <p className='text-xl md:text-left text-center font-bold'>Upstream Copper Raw Materials</p>
-                <p className='text-lg py-2'>We source and trade:</p>
-                <div className='p-4 bg-white rounded-2xl'>
-                 <ul className='list-disc p-2 mx-2 ' >             
-                   <li ><span className='font-bold py-2 '>Copper Ores: </span> <span className='text-sm'>Mined from top locations like Africa and processed into concentrates.</span></li>
-                   <li><span className='font-bold py-2'>Copper Concentrates: </span> <span className='text-sm'>Graded at 15-35% copper content for smelting and refining. </span></li>
-                   <li ><span className='font-bold py-2'>Copper Blister: </span> <span className='text-sm'>Produced at 90-95% purity for manufacturing copper anodes.</span></li>
-                   <li><span className='font-bold py-2'>Copper Cathodes: </span> <span className='text-sm'>Electrolytic copper (99.99% pure) traded globally</span></li>
-                 </ul> 
-                 </div>
-              </div>
-            
-              <div className='p-4 m-2 '>
-               <Image src="/images/upstream.avif" width={600} height={600} alt="copper"/>
-
-              </div>
-            </div>
+      <div className='lg:py-20 lg:mx-20 md:m-10 m-8 flex flex-col sm:flex-row md:gap-4 bg-Primary-1/10 rounded-[20px]'>
 
 
-            <div className='flex flex-col lg:w-1/2 bg-gray-100 p-8 rounded-2xl '>
-              <div className='lg:p-8'> 
-                <p className='text-xl md:text-left text-center font-bold'>Downstream Copper Products</p>
-                <p className='text-lg py-2'>Our partnerships extend to manufacturers and distributors of:</p>
-                <div className='p-4 bg-white rounded-2xl'>
-                 <ul className='list-disc p-2 mx-2' >             
-                   <li><span className='font-bold py-2'>Copper Rods:  </span> <span className='text-sm'>Electrolytic Tough Pitch (ETP) and Oxygen-Free Copper (OFC) rods in various sizes for electrical and industrial applications.</span></li>
-                   <li><span className='font-bold py-2'>Copper Wires: </span> <span className='text-sm'>From fine domestic wires to heavy-duty armoured cables.</span></li>
-                   <li><span className='font-bold py-2'>Copper Extrusions:  </span> <span className='text-sm'>Including busbars, strips, and profiles for transformers, switchgears, and other electrical systems.</span></li>
-                   <li><span className='font-bold py-2'>Ultra-High Purity Copper:  </span> <span className='text-sm'>6N copper (99.9999%) for critical applications in semiconductors, solar cells, and medical devices.</span></li>
-                 </ul> 
-                 </div>
-              </div>
-            
-              <div className='p-4 m-2 '>
-               <Image src="/images/upstream.jpg" width={800} height={800} alt="copper"/>
-
-              </div>
-            </div>
-
-
-          </div>
+      <div className='flex lg:w-1/2 p-8 items-center rounded-2xl'>
+        <Image
+                src="/images/copper-products.jpg"
+                width={600}
+                height={600}
+                alt="copper image"
+              />
         </div>
+
+        <div className='lg:w-1/2  lg:p-20 p-4 m-2 gap-8 flex flex-col justify-center'>
+          <div className='flex gap-4 flex-col dark:text-gray-700'>
+            <h2 className='text-5xl font-bold lg:text-left text-center'>Products We Supply</h2>
+            <h4 className='text-xl lg:text-left text-center'>We specialize in the distribution of a wide range of medical equipment and supplies, including:</h4>
+            <ul className='list-disc p-2 mx-2'>
+                   <li ><span className='font-bold py-2 '>Diagnostic Equipment:  </span> <span className='text-sm'>X-Ray Machines, Ultrasound Systems, MRI and CT Scanners, Blood Testing Machines</span></li>
+                   <li><span className='font-bold py-2'>Hospital and Clinic Essentials: </span> <span className='text-sm'>Hospital Beds and Furniture, Patient Monitors, Ventilators, Sterilization Equipment </span></li>
+                   <li ><span className='font-bold py-2'>Surgical Instruments: </span> <span className='text-sm'>General Surgery Kits, Orthopedic Surgery Tools, Cardiovascular Surgical Instruments.</span></li>
+                   <li><span className='font-bold py-2'>Personal Protective Equipment &#40;PPE&#41;: </span> <span className='text-sm'>Surgical Masks and Respirators, Medical Gloves, Protective Gowns and Face Shields</span></li>
+                   <li><span className='font-bold py-2'>Consumables and Disposables : </span> <span className='text-sm'>Syringes and Needles, IV Bags and Tubing, Wound Care Products</span></li>
+                   <li><span className='font-bold py-2'>Pharmaceutical Refrigeration : </span> <span className='text-sm'>Vaccine Storage Units, Blood Bank Refrigerators</span></li>
+                 </ul>
+          </div>          
+        </div>
+        
 
         
 
@@ -127,55 +97,64 @@ const page = () => {
 
       <div className='md:m-20 m-8 flex flex-col sm:flex-row md:gap-4 rounded-xl '>        
 
-        <div className='p-4 m-2 md:w-1/2'>
+        
+
+        <div className='md:p-20 p-4 m-2 gap-4 md:w-1/2 flex flex-col justify-center'>
+          <h2 className='text-5xl font-bold md:text-left text-center'>Healthcare Services</h2>
+          <p className='md:text-left text-center'>In addition to supplying medical equipment, we offer comprehensive services to ensure the effective deployment and use of our products:</p>
+          <ul className='list-disc p-2 mx-2'>
+                   <li ><span className='font-bold py-2 '>Logistics and Delivery:  </span> <span className='text-sm'>Efficient shipping across Africa and the Middle East, ensuring timely delivery even in remote locations.</span></li>
+                   <li><span className='font-bold py-2'>Installation and Maintenance: </span> <span className='text-sm'>On-site installation and regular maintenance by our trained technicians. </span></li>
+                   <li ><span className='font-bold py-2'>Training and Support: </span> <span className='text-sm'>Hands-on training for healthcare professionals on using advanced medical devices; 24/7 customer support to resolve technical issues.</span></li>
+                   <li><span className='font-bold py-2'>Customized Solutions: </span> <span className='text-sm'>Tailored packages to meet the specific needs of hospitals, clinics, and NGOs operating in under-resourced areas.</span></li>
+                   
+                 </ul>
+        </div>
+
+        <div className='lg:my-20 p-4 m-2 md:w-1/2'>
          <Image src="/images/resize-sorting-metal.png" width={961} height={761} alt="copper"/>
         </div>
-
-        <div className='md:p-20 p-4 m-2 gap-4 md:w-1/2 flex flex-col justify-center'>
-          <h2 className='text-5xl font-bold md:text-left text-center'>Recycling Copper for a Sustainable Future</h2>
-          <p className='md:text-left text-center'>Zencorp is committed to environmental stewardship. We recover and recycle end-of-life copper products and factory waste, contributing to a circular economy. With the infrastructure to handle large volumes, we aim to recover 20% of copper raw materials that would otherwise end up in landfills.</p>
-        </div>
-
-        
       </div>
 
-      <div className='md:m-20 m-8 flex flex-col sm:flex-row md:gap-4 rounded-xl bg-white rounded-2xl '>   
+      <div className='md:m-20 m-8 flex flex-col sm:flex-row md:gap-4 rounded-xl bg-white rounded-2xl dark:text-gray-700'>  
 
-        <div className='md:p-20 p-4 m-2 gap-4 md:w-1/2 flex flex-col justify-center'>
-          <h2 className='text-5xl font-bold md:text-left text-center'>Industries We Serve</h2>
-          
-          <p className='md:text-left text-center'>Zencorp supplies copper products to a wide range of sectors, including:</p>
+        <div className='p-4 m-2 md:w-1/2 flex items-center justify-center'>
+         <Image src="/images/ev-car.jpg" width={961} height={761} alt="copper"/>
+        </div>     
+
+        <div className='md:p-20 lg:py-20 p-4 m-2 gap-4 md:w-1/2 flex flex-col justify-center'>
+          <h2 className='text-5xl font-bold md:text-left text-center'>Our Impact</h2>          
           <div className='p-4 bg-white rounded-2xl'>
                  <ul className='list-disc p-2 mx-2' >             
-                   <li><span className='font-bold py-2'>Power Systems & Utilities:   </span> <span className='text-sm'>Serving giants like ABB, Siemens, and GE.</span></li>
-                   <li><span className='font-bold py-2'>Electric Vehicles: </span> <span className='text-sm'>Supporting EV innovation with advanced materials.</span></li>
-                   <li><span className='font-bold py-2'>Semiconductors and Electronics:  </span> <span className='text-sm'>Delivering ultra-high purity copper for precision applications.</span></li>
-                   <li><span className='font-bold py-2'>Construction:  </span> <span className='text-sm'>Providing durable copper solutions for wiring and infrastructure.</span></li>
+                   <li><span className='font-bold py-2'>Improved Healthcare Access:   </span> <span className='text-sm'>By delivering high-quality medical equipment, we empower healthcare providers to offer better diagnostic and treatment options.</span></li>
+                   <li><span className='font-bold py-2'>Strengthened Health Systems: </span> <span className='text-sm'>Our products and services play a key role in building resilient healthcare infrastructures in underserved regions.</span></li>
+                   <li><span className='font-bold py-2'>Saving Lives:  </span> <span className='text-sm'>From life-support equipment to diagnostic tools, our solutions ensure that patients receive timely and effective care.</span></li>
+                   
                  </ul> 
                  </div>
         </div>     
 
-        <div className='p-4 m-2 md:w-1/2 flex items-center justify-center'>
-         <Image src="/images/ev-car.jpg" width={961} height={761} alt="copper"/>
-        </div>    
+        
 
         
       </div>
 
       <div className='md:m-20 m-8 flex flex-col sm:flex-row md:gap-4 '>        
 
-        <div className='p-4 m-2 flex items-center md:w-1/2'>
-         <Image src="/images/africa-map.jpg" width={961} height={761} alt="copper"/>
-        </div>
+        
 
         <div className='md:p-20 p-4 m-2 gap-4 md:w-1/2 flex flex-col justify-center'>
         <h2 className='text-5xl md:text-left text-center font-bold'>Why Choose Zencorp?</h2>
-        <ul className='list-disc'>
-        <li><p>Global Reach: Operations spanning MENA, Asia, Europe, and Africa.</p> </li>
-        <li><p>Sustainability Focus: Prioritizing recycling and eco-friendly practices.</p></li>
-        <li><p>Quality Assurance: Compliance with international standards for long-term partnerships. </p></li>
-        <li><p>Customer-Centric Approach: Tailored solutions to meet your specific needs. </p></li>
-        </ul>
+        <ul className='list-disc p-2 mx-2' >             
+                   <li><span className='font-bold py-2'>Reliable Quality: </span> <span className='text-sm'>We partner with globally trusted manufacturers to ensure all products meet international standards.</span></li>
+                   <li><span className='font-bold py-2'>Regional Expertise: </span> <span className='text-sm'>With extensive knowledge of the healthcare challenges in Africa and the Middle East, we offer solutions tailored to local needs.</span></li>
+                   <li><span className='font-bold py-2'>Comprehensive Solutions:  </span> <span className='text-sm'>From supply to service, we provide end-to-end healthcare solutions.</span></li>
+                   <li><span className='font-bold py-2'>Sustainability Focus:  </span> <span className='text-sm'>We aim to contribute to long-term health outcomes by offering durable, sustainable, and impactful products.</span></li>
+                 </ul> 
+        </div>
+
+        <div className='p-4 m-2 flex items-center md:w-1/2'>
+         <Image src="/images/africa-map.jpg" width={961} height={761} alt="copper"/>
         </div>
 
         
@@ -186,19 +165,7 @@ const page = () => {
          <Image src="/images/african-women-laptop1.jpg" width={1600} height={300} alt="copper"/>
         </div>  
 
-      <div className='md:m-20 m-8 flex flex-col sm:flex-row md:gap-4 rounded-xl '>
-
-        <div className='md:p-20 p-4 m-2 gap-4 md:w-1/2 flex flex-col justify-center'>
-          <h2 className='text-5xl font-bold md:text-left text-center'>Join the Copper Revolution</h2>
-          <p className='md:text-left text-center '>Whether you&#8217;re in need of raw materials, finished products, or recycling solutions, Zencorp is your gateway to the complete copper ecosystem. Let&#8217;s shape a brighter, more sustainable future together.</p>
-        </div>     
-
-        <div className='p-4 m-2 md:w-1/2'>
-         <Image src="/images/father-carry.jpg" width={961} height={761} alt="copper"/>
-        </div>  
-        
       
-      </div>
       <div className='flex justify-center'>
       {/**<button type='button' className='px-8 py-4 m-2 bg-Primary-1/70 rounded-md text-center text-Primary-1/10 hover:bg-Secondary-2/70'>Learn More</button> */}
       </div>
