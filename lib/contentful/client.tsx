d422e59a4556b759f7985c80f94aa27159587418
Ismail <dewdigitalgr@gmail.com>
2025-01-18 21:16:07 +0400

    Initial commit

--- lib/contentful/client.tsx
+++ lib/contentful/client.tsx
@@ -1,6 +1,10 @@
 import { createClient } from 'contentful';
 
+if (!process.env.NEXT_PUBLIC_CONTENTFUL_GRAPHQL_ENDPOINT || !process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN) {
+  throw new Error('Contentful environment variables are missing. Please check NEXT_PUBLIC_CONTENTFUL_GRAPHQL_ENDPOINT and NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN in your .env.local file.');
+}
+
 export const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_GRAPHQL_ENDPOINT.split('/')[5], // Extract the space ID from the URL
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
 });
