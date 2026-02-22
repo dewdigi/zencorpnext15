096f12b46bee5bf1d44d61bfce547e99d23fecf5
Ismail <dewdigitalgr@gmail.com>
2026-02-22 13:42:26 +0400

    Patch Next.js for Vercel CVE block and update contact map

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -85,7 +85,7 @@ const page = () => {
                              <br/>TRN: 100315782100003</p>
                             
                             <div className="mt-5">
-                                <Link href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin"
+                                <Link href="https://maps.google.com/?q=AL+KHABEESI+BLDG+Office+No3+Dubai+UAE"
                                 data-type="iframe" className="video-play-icon read-more lightbox text-orange-500 hover:text-orange-500 after:bg-orange-500 transition duration-500">View on Google map</Link>
                             </div>
                         </div>
@@ -96,7 +96,7 @@ const page = () => {
         <div className="container-fluid relative">
             <div className="grid grid-cols-1">
                 <div className="w-full leading-[0] border-0">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin" style={{border:"0"}} title="my-frame" className="w-full h-[500px]" allowFullScreen></iframe>
+                    <iframe src="https://maps.google.com/maps?q=AL+KHABEESI+BLDG+Office+No3+Dubai+UAE&output=embed" style={{border:"0"}} title="my-frame" className="w-full h-[500px]" allowFullScreen></iframe>
                 </div>
             </div>
         </div>
